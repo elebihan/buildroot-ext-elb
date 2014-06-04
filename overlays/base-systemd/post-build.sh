557d5df324db4ec12d1b98bed7c1a899eb24bdb6
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2014-06-04 12:42:45 +0200

    base-systemd: fix /var, /run, resolv.conf setup
    In order for systemd to run properly, the /var and /run directories of
    the target should be changed:
    
    - /run will be mounted and populated by systemd, thanks to the
      /usr/lib/tmpfiles.d/*.conf files.
    - /var/run and /var/lock should be symlinks to /run
    - /var/tmp *should not* be a symlink to /tmp, as it will be mounted and
      populated by systemd.
    
    Also set /etc/resolv.conf as a symlink to
    /run/systemd/network/resolv.conf if systemd-networkd is used.
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>

--- overlays/base-systemd/post-build.sh
+++ overlays/base-systemd/post-build.sh
@@ -11,6 +11,15 @@ RANDOM_SEED=${TARGET_DIR}/var/lib/random-seed
 echo "Pruning SysV leftovers"
 rm -rf ${TARGET_DIR}/etc/init.d
 
+echo "Updating /run, /var..."
+rm -f ${TARGET_DIR}/var/tmp
+rm -f ${TARGET_DIR}/var/run
+rm -f ${TARGET_DIR}/var/lock
+rm -rf ${TARGET_DIR}/run
+mkdir -p ${TARGET_DIR}/run
+ln -sf ../run ${TARGET_DIR}/var/run
+ln -sf ../run ${TARGET_DIR}/var/lock
+
 echo "Adding missing directories"
 mkdir_if_missing ${TARGET_DIR}/etc/dropbear
 mkdir_if_missing ${TARGET_DIR}/etc/systemd/system/sockets.target.wants
@@ -33,6 +42,7 @@ fi
 if grep -q -F "BR2_PACKAGE_SYSTEMD_NETWORKD=y" ${BR2_CONFIG}; then
 	rm -f ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service
 	ln -sf /lib/systemd/system/systemd-networkd.service ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/systemd-networkd.service
+	ln -sf /run/systemd/network/resolv.conf ${TARGET_DIR}/etc/resolv.conf
 else
 	# Enable dhcpcd
 	rm -f ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/systemd-networkd.service
